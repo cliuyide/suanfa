49f6dd9ddf7013591c6c501825158c8ce220f0de
RunningFat <zl1230456zl@126.com>
2018-06-05 23:54:10 +0800

    同构字符串

---
+++ src/main/zhangliang/isomorphicstrings/IsomorphicStrings.java
@@ -0,0 +1,39 @@
+package main.zhangliang.isomorphicstrings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isomorphic(String s, String t) {
+		if (s == null || t == null || s.length() != t.length()) {
+			return false;
+		}
+		boolean result = true;
+		Map<Character, Character> map1 = new HashMap<>();
+		Map<Character, Character> map2 = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			Character s1 = map1.get(s.charAt(i));
+			Character t1 = map2.get(t.charAt(i));
+			if (s1 == null) {
+				if (t1 != null) {
+					result = false;
+					break;
+				}
+				map1.put(s.charAt(i), t.charAt(i));
+				map2.put(t.charAt(i), s.charAt(i));
+			} else {
+				if (t1 == null || t1 != s.charAt(i)) {
+					result = false;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+}
