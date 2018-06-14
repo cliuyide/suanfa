45c7ef103903db96ec0556ea5914b62e6a18b334
weirhp <weirhp@gmail.com>
2018-06-15 07:38:34 +0800

    Create moveZeroes.java

---
+++ src/main/houpo/moveZeroes/moveZeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nowIndex = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0){
+                nums[nowIndex] = nums[i];
+                nowIndex ++;
+            }
+        }
+        for(int j=nowIndex;j<nums.length;j++){
+            nums[j] = 0;
+        }
+    }
+}
