7b05fa02db06e3ca05041c6bf67748f6b7b6e2a6
RunningFat <zl1230456zl@126.com>
2018-06-15 14:09:20 +0800

    Move zeros

--- src/main/zhangliang/movezeros/MoveZeros.java
+++ src/main/zhangliang/movezeros/MoveZeros.java
@@ -6,11 +6,11 @@ package main.zhangliang.movezeros;
 public class MoveZeros {
 
     /**
-     * 21 / 21 test cases passed, Runtime: 3 ms
+     * 21 / 21 test cases passed, Runtime: 3 ms, 计算非0数的位移
      */
     public void moveZeroes(int[] nums) {
         if (nums != null) {
-            int displacement = 0;
+            int displacement = 0;// 位移
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] == 0) {
                     displacement++;
@@ -18,6 +18,24 @@ public class MoveZeros {
                     nums[i - displacement] = nums[i];
                     nums[i] = 0;
                 }
+            }
+        }
+    }
+
+    /**
+     * 参考他人，实际就把前pos个位置插入非0，剩余位置插入0；
+     */
+    public void moveZeroes2(int[] nums) {
+        if (nums != null) {
+            int pos = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[pos] = nums[i];
+                    pos++;
+                }
+            }
+            for (int j = pos; j < nums.length; j++) {
+                nums[j] = 0;
             }
         }
     }
