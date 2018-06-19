501957ea535396634730f7f0c8c1027d18b34e70
934392789 <934392789@qq.com>
2018-06-20 00:02:46 +0800

    111

--- src/main/liuyi/numsubarrayproductlessthank/NumSubarrayProductLessThanK4.java
+++ src/main/liuyi/numsubarrayproductlessthank/NumSubarrayProductLessThanK4.java
@@ -3,20 +3,17 @@ package main.liuyi.numsubarrayproductlessthank;
 import java.util.Date;
 
 /**
- * TODO 超时！！
+ * TODO 不是特别明晰
  * https://leetcode.com/problems/subarray-product-less-than-k/description/
  * @author liuyi
  *
  */
-public class NumSubarrayProductLessThanK3 {
-	public int count;
+public class NumSubarrayProductLessThanK4 {
 	public static void main(String[] args) {
         Date start = new Date();
-        NumSubarrayProductLessThanK3 a = new NumSubarrayProductLessThanK3();
-		System.out.println(a
+        System.out.println(new NumSubarrayProductLessThanK4()
                 .numSubarrayProductLessThanK(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 9));
         System.out.println(System.currentTimeMillis() - start.getTime());
-        System.out.println();
 	}
 	public int numSubarrayProductLessThanK(int[] nums, int k) {
         int result = 0;
@@ -24,27 +21,15 @@ public class NumSubarrayProductLessThanK3 {
 			return result;
 		}
         // 外圈循环,从第几个数开始
+        int p = 1;
+        int left=0;
         for (int i = 0; i < nums.length; i++) {
-            int p = 1;
-            // 乘到第几个数
-            int j = i;
-            for (; j < nums.length && p < k; j++) {
-            	count++;
-                p *= nums[j];
-            }
-            if(i==0&&j==nums.length&&p==1){
-            	for(i = 0; i < nums.length; i++){
-            		count++;
-            		result += nums.length-i;
-            	}
-            }else{
-            	if (p < k) {
-                    result += j - i;
-                } else {
-                    result += j - i - 1;
-                }
-            } 
-
+        	p*=nums[i];
+        	while(p>=k){
+        		p/=nums[left];
+        		left++;
+        	}
+        	result+=(i-left + 1);
 		}
 		return result;
         
