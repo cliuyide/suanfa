3edde05662ea5c0202af4528c75c418b320e23cc
RunningFat <zl1230456zl@126.com>
2018-06-19 18:01:17 +0800

    子数组乘机小于K

---
+++ src/main/zhangliang/subarrayproduct/LessThanK.java
@@ -0,0 +1,76 @@
+package main.zhangliang.subarrayproduct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LessThanK {
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (nums == null || k == 0) {
+            return 0;
+        }
+        int count = 0;
+        long product = 1;
+        Integer lastIndex = null;// 从上一个起点开始的最长符合要求的串
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int startIndex = 0; startIndex < nums.length; startIndex++) {
+            // 上一个起点
+            lastIndex = map.get(startIndex - 1);
+            // 小于0，起点大于k
+            if (lastIndex == null || lastIndex < 0) {
+                product = 1;
+                for (int sublength = 1; sublength <= nums.length - startIndex; sublength++) {
+                    product = product * nums[startIndex + sublength - 1];
+                    if (product < k) {
+                        count++;
+                    } else {
+                        map.put(startIndex, startIndex + sublength - 2);
+                        product = product / nums[startIndex + sublength - 1];
+                        break;
+                    }
+                    if (sublength == nums.length - startIndex) {
+                        map.put(startIndex, startIndex + sublength - 1);
+                    }
+                }
+            }
+            // 可以省略一些操作
+            else {
+                count += subCount(startIndex, lastIndex);
+                product = product / nums[startIndex - 1];
+                map.put(startIndex, lastIndex);
+                for (int sublength = lastIndex - startIndex + 2; sublength <= nums.length - startIndex; sublength++) {
+                    product = product * nums[startIndex + sublength - 1];
+                    if (product < k) {
+                        count++;
+                    } else {
+                        map.put(startIndex, startIndex + sublength - 2);
+                        break;
+                    }
+                    if (sublength == nums.length - startIndex) {
+                        map.put(startIndex, startIndex + sublength - 1);
+                    }
+                }
+                if (lastIndex == nums.length - 1) {
+                    break;
+                }
+            }
+
+        }
+        return count;
+    }
+
+    public static int subCount(int start, int end) {
+        if (start < 0 || end < 0 || end < start) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 1; i <= end - start + 1; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LessThanK().numSubarrayProductLessThanK(new int[] { 10, 5, 2, 6 }, 100));
+    }
+}
