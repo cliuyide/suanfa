7cadf4aa026b3c385f185263c70d97b90d07cf20
RunningFat <zl1230456zl@126.com>
2018-06-27 00:41:00 +0800

    partition to k

--- src/main/zhangliang/partitiontok/EqualSumSubsets.java
+++ src/main/zhangliang/partitiontok/EqualSumSubsets.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class EqualSumSubsets {
 	/**
-	 * TODO
+	 * TODO 未完待续
 	 * 
 	 * @param nums
 	 * @param k
@@ -25,10 +25,12 @@ public class EqualSumSubsets {
 		} else if (nums.length == 1 && k == 1) {
 			return true;
 		}
+		int[] arr = new int[10000];
 		Arrays.sort(nums);
 		int sum = 0;
 		for (int i = 0; i < nums.length; i++) {
 			sum += nums[i];
+			arr[nums[i]]++;
 		}
 		if (sum % k > 0) {
 			return false;
@@ -37,7 +39,38 @@ public class EqualSumSubsets {
 		if (nums[nums.length - 1] > target) {
 			return false;
 		}
+		int leftnum = nums.length;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			if (arr[nums[i]] > 0) {
+				sum = nums[i];
+				arr[nums[i]]--;
+				leftnum--;
+				int j = nums.length - 1;
+				while (sum < target && j >= 0) {
+					if (arr[nums[j]] > 0) {
+						sum += nums[j];
+						if (sum <= target) {
+							arr[nums[j]]--;
+							leftnum--;
+						} else {
+							sum -= nums[j];
+						}
+					}
+					j--;
+				}
+				if (sum != target) {
+					return false;
+				}
+			}
+		}
 
-		return true;
+		return leftnum == 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(System.currentTimeMillis());
+		System.out.println(new EqualSumSubsets().canPartitionKSubsets(
+				new int[] { 3522, 181, 521, 515, 304, 123, 2512, 312, 922, 407, 146, 1932, 4037, 2646, 3871, 269 }, 5));
+		System.out.println(System.currentTimeMillis());
 	}
 }
