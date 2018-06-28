e244e7a5d26a8449deea0989b042f1475c0c1b4f
RunningFat <zl1230456zl@126.com>
2018-06-29 00:23:42 +0800

    数组去重

--- src/main/zhangliang/removeduplicates/RemoveDuplicates.java
+++ src/main/zhangliang/removeduplicates/RemoveDuplicates.java
@@ -15,11 +15,11 @@ public class RemoveDuplicates {
 	 * @return
 	 */
 	public int removeDuplicates(int[] nums) {
-		int index = 0;
 		if (nums == null || nums.length == 0)
 			return 0;
 		if (nums.length == 1)
 			return 1;
+		int index = 0;
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] != nums[index])
 				nums[++index] = nums[i];
