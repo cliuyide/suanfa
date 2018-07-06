9bd154c0c8c089da92072083053723c320ed2891
RunningFat <zl1230456zl@126.com>
2018-07-06 13:56:18 +0800

    best time to buy and sell stock II

--- src/main/zhangliang/besttimetobuy/SellStock.java
+++ src/main/zhangliang/besttimetobuy/SellStock.java
@@ -60,7 +60,7 @@ public class SellStock {
         }
         int profit = 0;
         int minBuy = 0;
-        for (int i = 0; i < prices.length; i++) {
+        for (int i = 1; i < prices.length; i++) {
             if (prices[i] < prices[minBuy]) {
                 minBuy = i;
             } else if (prices[i] - prices[minBuy] > profit) {
---
+++ src/main/zhangliang/besttimetobuy/SellStockII.java
@@ -0,0 +1,33 @@
+package main.zhangliang.besttimetobuy;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ */
+public class SellStockII {
+    /**
+     * 201 / 201 test cases passed.Runtime: 1 ms
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int profit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - minPrice > profit) {
+                profit = prices[i] - minPrice;
+            } else {
+                minPrice = prices[i];
+                if (profit > 0) {
+                    maxProfit += profit;
+                    profit = 0;
+                }
+            }
+        }
+        if (profit > 0) {
+            maxProfit += profit;
+        }
+        return maxProfit;
+    }
+}
