90565886c73213672564b62a9c2fcb29d06bff41
RunningFat <zl1230456zl@126.com>
2018-07-06 14:00:50 +0800

    best time to buy and sell stock II

--- src/main/zhangliang/besttimetobuy/SellStockII.java
+++ src/main/zhangliang/besttimetobuy/SellStockII.java
@@ -30,4 +30,20 @@ public class SellStockII {
         }
         return maxProfit;
     }
+
+    /**
+     * 201 / 201 test cases passed.Runtime: 1 ms
+     */
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
 }
