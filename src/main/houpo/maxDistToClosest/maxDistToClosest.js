e26540d3ecbaebb36115220aa6980701e4108a55
weirhp <weirhp@gmail.com>
2018-07-13 00:45:44 +0800

    Update maxDistToClosest.js

--- src/main/houpo/maxDistToClosest/maxDistToClosest.js
+++ src/main/houpo/maxDistToClosest/maxDistToClosest.js
@@ -10,21 +10,20 @@ var maxDistToClosest = function(seats) {
     for(var i=0;i<seats.length;i++){
         if(seats[i] == 0 && i !=seats.length-1){
             continue;
-        } else {
-            endPos = i;
-            if(i ==seats.length-1 && seats[i]==0){
-                endPos ++;
-            }
-            var tmpMax = 0;
-            if(startPos == 0 || endPos==seats.length){
-                tmpMax = endPos - startPos ;
-            } else {
-                tmpMax = Math.floor((endPos - startPos )/2.0 + 0.5)
-            }
-            if(tmpMax>max)
-                max = tmpMax;
-            startPos = i + 1;
         } 
+        endPos = i;
+        if(i ==seats.length-1 && seats[i]==0){
+            endPos ++;
+        }
+        var tmpMax = 0;
+        if(startPos == 0 || endPos==seats.length){
+            tmpMax = endPos - startPos ;
+        } else {
+            tmpMax = Math.floor((endPos - startPos )/2.0 + 0.5)
+        }
+        if(tmpMax>max)
+            max = tmpMax;
+        startPos = i + 1;
     }
     return max;
     
