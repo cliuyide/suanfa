152a2fc48389a8dd9c462870b8e7d7edaf628d2e
weirhp <weirhp@gmail.com>
2018-07-13 00:44:00 +0800

    Create maxDistToClosest.js

---
+++ src/main/houpo/maxDistToClosest/maxDistToClosest.js
@@ -0,0 +1,31 @@
+/**
+https://leetcode-cn.com/problems/maximize-distance-to-closest-person/description/
+ * @param {number[]} seats
+ * @return {number}
+ */
+var maxDistToClosest = function(seats) {
+    var max = 1;
+    var startPos = 0;
+    var endPos = 0;
+    for(var i=0;i<seats.length;i++){
+        if(seats[i] == 0 && i !=seats.length-1){
+            continue;
+        } else {
+            endPos = i;
+            if(i ==seats.length-1 && seats[i]==0){
+                endPos ++;
+            }
+            var tmpMax = 0;
+            if(startPos == 0 || endPos==seats.length){
+                tmpMax = endPos - startPos ;
+            } else {
+                tmpMax = Math.floor((endPos - startPos )/2.0 + 0.5)
+            }
+            if(tmpMax>max)
+                max = tmpMax;
+            startPos = i + 1;
+        } 
+    }
+    return max;
+    
+};
