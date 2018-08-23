d2ed87eee85e045e9e56844828aae3c492662c7e
934392789 <934392789@qq.com>
2018-08-23 11:31:54 +0800

    之前做的太次了

--- src/main/liuyi/binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
+++ src/main/liuyi/binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -1,15 +1,22 @@
-package liuyi.binaryTreeLevelOrderTraversal;
+package main.liuyi.binaryTreeLevelOrderTraversal;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
  class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
 }
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ * 
+ * @author liuyi
+ *
+ */
 public class BinaryTreeLevelOrderTraversal {
 	public static void main(String[] args) {
 		TreeNode t1=new TreeNode(3);
@@ -21,40 +28,35 @@ public class BinaryTreeLevelOrderTraversal {
 		t1.right=t3;
 		t3.left=t4;
 		t3.right=t5;
-		new BinaryTreeLevelOrderTraversal().levelOrder(t1);
+		System.out.println(JSON.toJSONString(new BinaryTreeLevelOrderTraversal().levelOrder(t1)));
 	}
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result=new ArrayList<List<Integer>>();
         if(root==null){
             return result;
         }
-        Map<Integer,List<Integer>> map=new HashMap<Integer, List<Integer>>();
-        handler(root,0,map);
-        map.entrySet().forEach(entry->result.add(entry.getValue()));
+		handler(root, 0, result);
         return result;
     }
     
-    public void handler(TreeNode root,Integer depth,Map<Integer,List<Integer>> map){
+	public void handler(TreeNode root, Integer depth, List<List<Integer>> result) {
+		List<Integer> list = null;
+		if (result.size() > depth) {
+			list = result.get(depth);
+		} else {
+			list = new ArrayList<>();
+			result.add(list);
+		}
         if(root.left==null&&root.right==null){
-        	List<Integer> list=map.get(depth);
-        	if(list==null){
-        		list=new ArrayList<Integer>(){};
-        		map.put(depth, list);
-        	}
         	list.add(root.val);
         	return;
         }
         if(root.left!=null){
-            handler(root.left,depth+1,map);
+			handler(root.left, depth + 1, result);
         }
         if(root.right!=null){
-            handler(root.right,depth+1,map);
+			handler(root.right, depth + 1, result);
         }
-        List<Integer> list=map.get(depth);
-    	if(list==null){
-    		list=new ArrayList<Integer>(){};
-    		map.put(depth, list);
-    	}
     	list.add(root.val);
     }
 }
